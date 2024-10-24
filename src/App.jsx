28ad25a70ea9878b8f84580f014f15a3897bc4b4
smallproject <j.a.villas@live.com.ph>
2024-10-24 22:35:52 +0200

    added buttons and pokemon component to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import logo from './assets/img.png'
+import PokemonDetail from "./components/Pokemon-Detail/Pokemon-Detail.jsx";
 
 function App() {
 
   return (
     <>
-      <h1>Gotta catch em all!</h1>
+        <img src={logo} alt="pokemon logo" width={"550px"}/>
+        <section>
+            <button>Vorige</button>
+            <button>Volgende</button>
+        </section>
+
+        <PokemonDetail />
+
     </>
   )
 }
